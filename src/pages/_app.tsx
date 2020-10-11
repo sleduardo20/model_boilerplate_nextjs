d5e9aae207e00395922769d50f1d49d0a5b2b876
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-11 01:17:18 -0300

    config finish model boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+
 import GlobalStyles from 'styles/GlobalStyles';
 
 function App({ Component, pageProps }: AppProps) {
@@ -9,10 +10,10 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work TypeScript, React, 
-          NextJS and Styled Components"
+          content="A simple project starter to work TypeScript, React,NextJS and Styled Components"
         />
       </Head>
       <Component {...pageProps} />
