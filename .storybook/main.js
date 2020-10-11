a06c57eb15370c595c564388f08bf2bedf44185f
sleduardo20 <sl.eduardo20@gmail.com>
2020-10-10 23:55:12 -0300

    config finish storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,5 @@
 module.exports = {
   "stories": ["../src/components/**/stories.tsx"],
-  "addons": [
-    '@storybook/addon-links',
-    '@storybook/addon-actions',
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs"
-  ],
+  "addons": ["@storybook/addon-essentials"],
   
 }
